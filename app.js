c6b7e0b47995bb7d45847a9722ac4a7219742027
João Victor de Medeiros <medeirosjoaoadv@gmail.com>
2021-07-07 03:13:41 -0300

    Inicio

--- app.js
+++ app.js
@@ -1,5 +1,189 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const movies = require("./movies.js");
+const commands = require("./commands.js");
 
-const Discord = require('discord.js')
-const bot = new Discord.Client()
+receiveMessage = (message) => {
+  const authorMsg = message.author.username;
+  const msg = new Discord.MessageEmbed();
+  if (message.author.bot == false) {
+    if (message.content == "!comandos") {
+      message.channel.send(
+        `Olá! Seja muito bem vindo, ${authorMsg}. Abaixo você verá a minha lista completa de comandos:`
+      );
+      msg.setTitle("LISTA DE COMANDOS");
+      msg.setColor("#f20c0c");
+      msg.setDescription(commands);
+      message.channel.send(msg);
+    } else if (message.content == "!starwars") {
+      message.channel.send(
+        "Essa é uma lista de todos os filmes em ordem de lançamento das trilogias e por ultimo os filmes Spin-off!"
+      );
+      movies.map((movie) => {
+        msg.setTitle(movie.name);
+        msg.setDescription(movie.synopsis);
+        msg.setColor("#00de3f");
+        msg.setThumbnail(movie.image);
+        msg.setImage(movie.image);
+        message.channel.send(msg);
+      });
+    } else if (message.content == "!starwars1") {
+      message.channel.send(
+        "Este é o primeiro filme lançado da primeira trilogia."
+      );
+      movies
+        .filter((movie) => movie.id == 1)
+        .map((movie) => {
+          msg.setTitle(movie.name);
+          msg.setDescription(movie.synopsis);
+          msg.setColor("#2127d9");
+          msg.setThumbnail(movie.image);
+          msg.setImage(movie.image);
+          message.channel.send(msg);
+        });
+    } else if (message.content == "!starwars2") {
+      message.channel.send(
+        "Este é o segundo filme lançado da primeira trilogia."
+      );
+      movies
+        .filter((movie) => movie.id == 2)
+        .map((movie) => {
+          msg.setTitle(movie.name);
+          msg.setDescription(movie.synopsis);
+          msg.setColor("#2127d9");
+          msg.setThumbnail(movie.image);
+          msg.setImage(movie.image);
+          message.channel.send(msg);
+        });
+    } else if (message.content == "!starwars3") {
+      message.channel.send(
+        "Este é o terceiro filme lançado da primeira trilogia."
+      );
+      movies
+        .filter((movie) => movie.id == 3)
+        .map((movie) => {
+          msg.setTitle(movie.name);
+          msg.setDescription(movie.synopsis);
+          msg.setColor("#2127d9");
+          msg.setThumbnail(movie.image);
+          msg.setImage(movie.image);
+          message.channel.send(msg);
+        });
+    } else if (message.content == "!starwars4") {
+      message.channel.send(
+        "Este é o primeiro filme lançado da segunda trilogia."
+      );
+      movies
+        .filter((movie) => movie.id == 4)
+        .map((movie) => {
+          msg.setTitle(movie.name);
+          msg.setDescription(movie.synopsis);
+          msg.setColor("#e3dc12");
+          msg.setThumbnail(movie.image);
+          msg.setImage(movie.image);
+          message.channel.send(msg);
+        });
+    } else if (message.content == "!starwars5") {
+      message.channel.send(
+        "Este é o segundo filme lançado da segunda trilogia."
+      );
+      movies
+        .filter((movie) => movie.id == 5)
+        .map((movie) => {
+          msg.setTitle(movie.name);
+          msg.setDescription(movie.synopsis);
+          msg.setColor("#e3dc12");
+          msg.setThumbnail(movie.image);
+          msg.setImage(movie.image);
+          message.channel.send(msg);
+        });
+    } else if (message.content == "!starwars6") {
+      message.channel.send(
+        "Este é o terceiro filme lançado da segunda trilogia."
+      );
+      movies
+        .filter((movie) => movie.id == 6)
+        .map((movie) => {
+          msg.setTitle(movie.name);
+          msg.setDescription(movie.synopsis);
+          msg.setColor("#e3dc12");
+          msg.setThumbnail(movie.image);
+          msg.setImage(movie.image);
+          message.channel.send(msg);
+        });
+    } else if (message.content == "!starwars7") {
+      message.channel.send(
+        "Este é o primeiro filme lançado da terceira trilogia"
+      );
+      movies
+        .filter((movie) => movie.id == 7)
+        .map((movie) => {
+          msg.setTitle(movie.name);
+          msg.setDescription(movie.synopsis);
+          msg.setColor("#ed0ff5");
+          msg.setThumbnail(movie.image);
+          msg.setImage(movie.image);
+          message.channel.send(msg);
+        });
+    } else if (message.content == "!starwars8") {
+      message.channel.send(
+        "Este é o segundo filme lançado da terceira trilogia"
+      );
+      movies
+        .filter((movie) => movie.id == 8)
+        .map((movie) => {
+          msg.setTitle(movie.name);
+          msg.setDescription(movie.synopsis);
+          msg.setColor("#ed0ff5");
+          msg.setThumbnail(movie.image);
+          msg.setImage(movie.image);
+          message.channel.send(msg);
+        });
+    } else if (message.content == "!starwars9") {
+      message.channel.send(
+        "Este é o terceiro filme lançado da terceira trilogia"
+      );
+      movies
+        .filter((movie) => movie.id == 9)
+        .map((movie) => {
+          msg.setTitle(movie.name);
+          msg.setDescription(movie.synopsis);
+          msg.setColor("#ed0ff5");
+          msg.setThumbnail(movie.image);
+          msg.setImage(movie.image);
+          message.channel.send(msg);
+        });
+    } else if (message.content == "!starwars10") {
+      message.channel.send("Este é o primeiro filme Spin-off");
+      movies
+        .filter((movie) => movie.id == 10)
+        .map((movie) => {
+          msg.setTitle(movie.name);
+          msg.setDescription(movie.synopsis);
+          msg.setColor("#f58002");
+          msg.setThumbnail(movie.image);
+          msg.setImage(movie.image);
+          message.channel.send(msg);
+        });
+    } else if (message.content == "!starwars11") {
+      message.channel.send("Este é o segundo filme Spin-off");
+      movies
+        .filter((movie) => movie.id == 11)
+        .map((movie) => {
+          msg.setTitle(movie.name);
+          msg.setDescription(movie.synopsis);
+          msg.setColor("#f58002");
+          msg.setThumbnail(movie.image);
+          msg.setImage(movie.image);
+          message.channel.send(msg);
+        });
+    }
+    message.channel.send(
+      `Fique à vontade para digitar o comando que quiser, ${authorMsg}.`
+    );
+  }
+};
 
-bot.login('ODYyMTEzMDk3Njk4NTA4ODAx.YOTnYw.oeGGHLZC_lqFVLcwQK1G6UNlyro');
+bot.on("message", receiveMessage);
+
+bot.login("ODYyMTEzMDE3MDc1ODU5NDU2.YOTnUA.G30bC3bZQgnHMQFp0p_7YG8fqmg");
---
+++ commands.js
@@ -0,0 +1,25 @@
+const commands = [`!starwars = mostra todos os filmes.
+
+!starwars1 = mostra o primeiro filme da primeira trilogia.
+
+!starwars2 = mostra o segundo filme da primeira trilogia.
+
+!starwars3 = mostra o terceiro filme da primeira trilogia.
+
+!starwars4 = mostra o primeiro filme da segunda trilogia.
+
+!starwars5 = mostra o segundo filme da segunda trilogia.
+
+!starwars6 = mostra o terceiro filme da segunda trilogia.
+
+!starwars7 = mostra o primeiro filme da terceira trilogia.
+
+!starwars8 = mostra o segundo filme da terceira trilogia.
+
+!starwars9 = mostra o terceiro filme da terceira trilogia.
+
+!starwars10 = mostra o primeiro filme Spin-off lançado.
+
+!starwars11 = mostra o segundo filme Spin-off lançado.`]
+
+module.exports = commands
