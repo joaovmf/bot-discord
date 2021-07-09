337b1702857ed3f7e811fe65d67a8746dc647ac0
João Victor de Medeiros <medeirosjoaoadv@gmail.com>
2021-07-09 13:42:15 -0300

    switch teste continua

--- controllers/switch.js
+++ controllers/switch.js
@@ -8,9 +8,7 @@ receiveMessage = (message) => {
     const authorMsg = author.username;
     switch (content) {
       case "!iniciar":
-        channel.send(
-          `Olá! Seja muito bem vindo, ${authorMsg}. Eu sou o bot do João. Abaixo você verá a minha lista completa de comandos:`
-        );
+        channel.send(`Olá! Seja muito bem vindo, ${authorMsg}. Eu sou o bot do João. Abaixo você verá a minha lista completa de comandos:`);
         const msg = new Discord.MessageEmbed();
         msg.setTitle("LISTA DE COMANDOS");
         msg.setColor("#4287f5");
@@ -18,11 +16,10 @@ receiveMessage = (message) => {
         channel.send(msg);
         console.log(content[9]);
         break;
-      case content.indexOf("!starwars") == 0:
+      case "!starwars":
+        content.indexOf("!starwars") == 0;
         if (content == "!starwars") {
-          channel.send(
-            "Essa é uma lista de todos os filmes em ordem de lançamento das trilogias e por ultimo os filmes Spin-off!"
-          );
+          channel.send("Essa é uma lista de todos os filmes em ordem de lançamento das trilogias e por ultimo os filmes Spin-off!");
           functions.showAll(channel);
         } else {
           functions.filterById(content[9], channel);
@@ -35,9 +32,7 @@ receiveMessage = (message) => {
         functions.filterByType(content.substring(1, 1000), channel);
         break;
       default:
-        channel.send(
-          `Este comando não é válido, ${authorMsg}. Por favor digite "!iniciar" para ver minha lista completa de comandos. Que a força esteja com você!`
-        );
+        channel.send(`Este comando não é válido, ${authorMsg}. Por favor digite "!iniciar" para ver minha lista completa de comandos. Que a força esteja com você!`);
     }
   }
 };
