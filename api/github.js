b0befb0bf9508bc9347e133c241454d49f4625a2
João Victor de Medeiros <medeirosjoaoadv@gmail.com>
2021-07-16 12:19:56 -0300

    github

--- api/github.js
+++ api/github.js
@@ -1,11 +1,11 @@
 const axios = require('axios')
 const { MessageEmbed } = require('discord.js')
 
-getGitHubByName = async () => {
-    const response = await axios.get(`https://api.github.com/users/${login}`)
+getGitHubByName = async (channel, username) => {
+    const response = await axios.get(`https://api.github.com/users/${username}`)
     const { data } = response
     channel.send(new MessageEmbed()
-    .setTitle(login)
+    .setTitle(username)
     .setURL(data.html_url)
     .setDescription(data.bio)
     .setFooter(`Repositórios Públicos:${data.public_repos}
