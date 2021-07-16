73074d098ad09e61d56755bae05a077d495b7a79
João Victor de Medeiros <medeirosjoaoadv@gmail.com>
2021-07-16 12:22:25 -0300

    name github

--- api/github.js
+++ api/github.js
@@ -5,7 +5,7 @@ getGitHubByName = async (channel, username) => {
     const response = await axios.get(`https://api.github.com/users/${username}`)
     const { data } = response
     channel.send(new MessageEmbed()
-    .setTitle(username)
+    .setTitle(data.name)
     .setURL(data.html_url)
     .setDescription(data.bio)
     .setFooter(`Repositórios Públicos:${data.public_repos}
