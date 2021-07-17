a7bfbdc1db619f33d9c7ff959878e3a5fc9312f0
João Victor de Medeiros <medeirosjoaoadv@gmail.com>
2021-07-17 14:09:35 -0300

    atualização wiki

--- api/wiki.js
+++ api/wiki.js
@@ -9,7 +9,7 @@ const getWiki = async (channel, search) => {
     const info = await page.info()
     const images = await page.images()
     const imageName = encodeURI(info.image.split(' ').join('_')).replace(/\(/g, '%28').replace(/\)/g, '%29')
-    channel.send(new MessageEmbed().setFooter(`Font: ${page.raw.fullurl}`).setURL(page.raw.fullurl).setTitle(info.name).setDescription(`${content[0].content.substring(0,4090)}...`).setImage(images.find(image => image.includes(imageName))))
+    channel.send(new MessageEmbed().setFooter(`Font: ${page.raw.fullurl}`).setURL(page.raw.fullurl).setTitle(info.name).setDescription(`${content[0].content.substring(0,3000)}...`).setImage(images.find(image => image.includes(imageName))))
   } catch (error) {
     channel.send(new MessageEmbed().setTitle('Resultado não encontrado').setDescription('Tente novamente com uma nova pesquisa'))
   }
